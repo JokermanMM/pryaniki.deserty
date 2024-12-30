19e8db8326f84dcd2a1c0711a278eeeb89251928
JokermanMM <45339027+JokermanMM@users.noreply.github.com>
2024-12-30 23:15:02 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return "Сервер работает!"
+
+if name == 'main':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
