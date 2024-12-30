50653b098088dbe4420a8abdb9a2334f3e2b5727
JokermanMM <45339027+JokermanMM@users.noreply.github.com>
2024-12-30 23:42:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
+from flask import Flask, request, jsonify
 import os
-from flask import Flask
 
 app = Flask(__name__)
+
+# Статистика переходов
+visits = {
+    "main": 0,
+    "telegram": 0,
+    "vk": 0,
+    "instagram": 0,
+    "whatsapp": 0
+}
 
 @app.route('/')
 def home():
     return "Сервер работает!"
+
+# Эндпоинт для увеличения количества переходов
+@app.route('/increment', methods=['POST'])
+def increment():
+    data = request.get_json()
+    name = data.get('name')
+
+    if name in visits:
+        visits[name] += 1
+        return jsonify({"message": f"Переход на {name} увеличен!", "visits": visits}), 200
+    else:
+        return jsonify({"message": "Неизвестный эндпоинт!"}), 400
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    global visits
+    visits = {key: 0 for key in visits}  # Сбрасываем все счётчики
+    return jsonify({"message": "Статистика сброшена!"}), 200
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
