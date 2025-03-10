122eaf2f47f27a4c900b526388c2b5fdba495997
Maksim Shner <45339027+JokermanMM@users.noreply.github.com>
2025-03-10 13:20:06 +0300

    +redis app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, redis
 import os
 
 app = Flask(__name__)
@@ -12,31 +12,33 @@ visits = {
     "whatsapp": 0
 }
 
-@app.route('/')
-def index():
-    return send_from_directory(os.getcwd(), 'index.html')
+# Подключаемся к Redis
+r = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'))
 
-# Эндпоинт для увеличения количества переходов
+# Функция для увеличения статистики
 @app.route('/increment', methods=['POST'])
 def increment():
     data = request.get_json()
     name = data.get('name')
 
-    if name in visits:
-        visits[name] += 1
-        return jsonify({"message": f"Переход на {name} увеличен!", "visits": visits}), 200
+    if name:
+        r.incr(name)  # Увеличиваем счётчик в Redis
+        return jsonify({"message": f"Переход на {name} увеличен!", "visits": get_visits()}), 200
     else:
         return jsonify({"message": "Неизвестный эндпоинт!"}), 400
 
-@app.route('/reset', methods=['POST'])
-def reset():
-    global visits
-    visits = {key: 0 for key in visits}  # Сбрасываем все счётчики
-    return jsonify({"message": "Статистика сброшена!"}), 200
+# Функция для получения статистики
+def get_visits():
+    visits = {key: int(r.get(key) or 0) for key in ["main", "telegram", "vk", "instagram", "whatsapp"]}
+    return visits
 
 @app.route('/statistics', methods=['GET'])
-def get_statistics():
-    return jsonify(visits), 200
+def statistics():
+    return jsonify(get_visits()), 200
+
+@app.route('/')
+def index():
+    return send_from_directory(os.getcwd(), 'index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
