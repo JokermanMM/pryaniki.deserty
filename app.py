8d60415b04d55fbda0a566d996f81283274b0744
Maksim Shner <45339027+JokermanMM@users.noreply.github.com>
2025-03-10 14:27:23 +0300

    app.py

--- app.py
+++ app.py
@@ -40,6 +40,6 @@ def get_visits():
     keys = r.keys()  # Получаем все ключи из Redis
     return {key: int(r.get(key) or 0) for key in keys}
 
-if name == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
